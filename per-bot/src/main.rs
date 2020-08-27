81e85685db35afdf94802f83fe95bc5c528e4fc3
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-08-27 13:36:17 +0100

    Extract tweet loading and updating

--- per-bot/src/main.rs
+++ per-bot/src/main.rs
@@ -1,42 +1,30 @@
 mod auth;
-mod tweets;
-mod opts;
 mod error;
+mod opts;
+mod tweets;
 
 use std::sync::Arc;
 
-use tokio;
+use anyhow::{Context, Result};
 use clap::Clap;
-use anyhow::{Result, Context};
 use redis::AsyncCommands;
 
 use auth::AuthConfig;
-use tweets::TweetList;
 use opts::*;
-
+use tweets::{Tweet, TweetList};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let opts = Options::parse();
-    let tweets = TweetList::load(opts.tweet_list).context("Error while reading tweet list")?;
-    let auth_conf = AuthConfig::load(opts.authentication).context("Error while reading authentication config")?;
+    update_redis(&opts).await?;
+    let auth_conf = AuthConfig::load(opts.authentication)
+        .context("Error while reading authentication config")?;
     let token = Arc::new(auth_conf.token());
 
-    let client = redis::Client::open(opts.redis_url)?;
-    let mut con = client.get_async_connection().await?;
-    for tweet in &tweets.tweet {
-        let _ : String = con.hset_multiple(
-            &tweet.id,
-            &[
-                ("message", &tweet.message),
-                ("interval", &tweet.interval.to_string()),
-                ("counter", &tweet.counter.to_string())
-            ]
-        ).await?;
-    }
+    let tl: Vec<Tweet> = Vec::new();
 
     let mut handlers = Vec::new();
-    for mut t in tweets.tweet {
+    for mut t in tl {
         let arc_tok = Arc::clone(&token);
         handlers.push(tokio::spawn(async move {
             let _ = t.send(arc_tok).await;
@@ -47,5 +35,24 @@ async fn main() -> Result<()> {
         h.await?;
     }
 
+    Ok(())
+}
+
+async fn update_redis(opts: &Options) -> Result<()> {
+    let tweets = TweetList::load(&opts.tweet_list).context("Error while reading tweet list")?;
+    let client = redis::Client::open(opts.redis_url.to_string())?;
+    let mut con = client.get_async_connection().await?;
+    for tweet in &tweets.tweet {
+        let _: String = con
+            .hset_multiple(
+                &tweet.id,
+                &[
+                    ("message", &tweet.message),
+                    ("interval", &tweet.interval.to_string()),
+                    ("counter", &tweet.counter.to_string()),
+                ],
+            )
+            .await?;
+    }
     Ok(())
 }
